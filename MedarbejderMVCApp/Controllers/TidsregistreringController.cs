c7febb0f3f1fac5a71a949748f5b3309c68b9264
jjohnsen123 <johnsenjeppe@gmail.com>
2024-12-05 11:27:32 +0100

    MVC app

---
+++ MedarbejderMVCApp/Controllers/TidsregistreringController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogic.BLL;
+using DTO.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MedarbejderMVCApp.Controllers
+{
+    public class TidsregistreringController : Controller
+    {
+        private MedarbejderBLL _medarbejderBLL = new MedarbejderBLL();
+        private SagBLL _sagBLL = new SagBLL();
+
+        // GET: Tidsregistrering/Create
+        public ActionResult Create(int medarbejderId)
+        {
+            var medarbejder = _medarbejderBLL.GetMedarbejder(medarbejderId);
+            ViewBag.Medarbejder = medarbejder;
+            ViewBag.Sager = new SelectList(_sagBLL.GetAllSager(), "Id", "Overskrift");
+            return View();
+        }
+
+        // POST: Tidsregistrering/Create
+        [HttpPost]
+        public ActionResult Create(TidsregistreringDTO tidsregistrering)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _medarbejderBLL.AddTidsregs(tidsregistrering.MedarbejderId, tidsregistrering);
+                    return RedirectToAction("Details", "Medarbejder", new { id = tidsregistrering.MedarbejderId });
+                }
+                ViewBag.Sager = new SelectList(_sagBLL.GetAllSager(), "Id", "Overskrift", tidsregistrering.SagId);
+                return View(tidsregistrering);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(tidsregistrering);
+            }
+        }
+    }
+}
